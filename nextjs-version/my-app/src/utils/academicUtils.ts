3ba67f2d45ba94d87c7c50c63b47536002a9accb
Vitaly Ford <5395250+vitalyford@users.noreply.github.com>
2025-12-28 12:15:58 -0500

    Updated how years are calculated

--- nextjs-version/my-app/src/utils/academicUtils.ts
+++ nextjs-version/my-app/src/utils/academicUtils.ts
@@ -2,7 +2,7 @@ import { publications } from "@/data/publications";
 
 export const getPublicationStats = () => {
     const totalPapers = publications.reduce((acc, pub) => acc + pub.papers.length, 0);
-    const yearsActive = publications.length;
+    const yearsActive = new Date().getFullYear() - 2007;
 
     return {
         totalPapers,
