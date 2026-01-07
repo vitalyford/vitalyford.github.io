e5401bb3b6dbf37d1f1e7896143939dd87b04f9c
Vitaly Ford <5395250+vitalyford@users.noreply.github.com>
2026-01-07 16:18:27 -0500

    Add NEXT_PUBLIC env vars for client-side

--- src/app/et/page.tsx
+++ src/app/et/page.tsx
@@ -1,5 +1,5 @@
 import { redirect } from "next/navigation";
 
 export default function ET() {
-  redirect(process.env.BOOKING_PAGE || "https://calendar.app.google/aHackGAhEtDC9EAW9");
+  redirect(process.env.BOOKING_PAGE || process.env.NEXT_PUBLIC_BOOKING_PAGE || "https://calendar.app.google/aHackGAhEtDC9EAW9");
 }
